6a859be3e489f135800ddaf3cbcf284681282ce3
tuvvakovv3-png <tuvvakovv3@gmail.com>
2026-04-28 11:19:58 +0500

    Update MainTest.java

--- test/MainTest.java
+++ test/MainTest.java
@@ -1,15 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
-public class MainTest {
-
-    @Test
-    void testSum() {
-        assertEquals(5, Main.sum(2, 3));
-    }
-
-    @Test
-    void testNegative() {
-        assertEquals(-1, Main.sum(2, -3));
+public class Main {
+    public static void main(String[] args) {
+        // Scanner арқылы System.in-нен деректерді оқимыз
+        Scanner scanner = new Scanner(System.in);
+        
+        // Пайдаланушы енгізген санды оқимыз
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            
+            // Жұп немесе тақ екенін анықтау (2-ге бөлінгендегі қалдық 0 болса - жұп)
+            if (number % 2 == 0) {
+                System.out.println("Even");
+            } else {
+                System.out.println("Odd");
+            }
+        }
+        
+        scanner.close();
     }
 }
